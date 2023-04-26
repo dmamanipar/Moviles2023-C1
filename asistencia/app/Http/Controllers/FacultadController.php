e690d7377a866383e07dca96f90384be5dae1d59
ivanBlade <itsesparta@gmail.com>
2023-04-26 15:12:11 -0500

    APLA

---
+++ asistencia/app/Http/Controllers/FacultadController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Facultad;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class FacultaControllerd extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+    public function __construct() {
+        $this->middleware('auth:api');
+        }
+
+    public function index()
+    {
+        Log::channel('stderr')->info("Si llega aqui");
+
+        $facultades=Facultad::all();
+        $mappedcollection = $facultades->map(function($facultad, $key) {
+        return [
+        'id' => $facultad->id,
+        'nombrefac' => $facultad->nombrefac,
+        'estado'=>$facultad->estado,
+        'iniciales'=>$facultad->iniciales,
+
+        ];
+        });
+        return response()->json(['success' => true,
+        'data' => $mappedcollection,
+        //'data' => Persona::all(),
+        'message' => 'lista de facultades'], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        Log::channel('stderr')->info($request);
+        Facultad::create($input);
+        return response()->json(['success' => true,
+        'data' => Facultad::all(),
+        'message' => 'Lista de Facultad'], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ asistencia/app/Models/Facultad.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Facultad extends Model
+{
+    use HasFactory;
+
+    protected $table = 'facultads';
+    public $timestamps = false;
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'id', 'nombrefac','estado','iniciales'
+    ];
+
+    public function escuelas()
+    {
+        return $this->hasMany(Escuela::class);
+    }
+}
