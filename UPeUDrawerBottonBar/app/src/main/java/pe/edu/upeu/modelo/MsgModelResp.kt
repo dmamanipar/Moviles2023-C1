7419a0a3b4028a36b2abc908713792f376108302
Alex-145 <linoalex145@gmail.com>
2023-05-03 15:57:57 -0500

    upgrade

--- UPeUDrawerBottonBar/app/src/main/java/pe/edu/upeu/modelo/MsgModelResp.kt
+++ UPeUDrawerBottonBar/app/src/main/java/pe/edu/upeu/modelo/MsgModelResp.kt
@@ -1,7 +1,10 @@
 package pe.edu.upeu.modelo
 
-data class MsgModelResp(
-    var success:Boolean,
-    var data:List<Actividad>,
-    var message:String
-)
+data class ComboModel(
+    val code: String,
+    val name: String
+    ): PickerValue() {
+    override fun searchFilter(query: String): Boolean {
+        return this.name.startsWith(query) } }
+abstract class PickerValue {
+    abstract fun searchFilter(query: String): Boolean }
---
+++ UPeUDrawerBottonBar/app/src/main/java/pe/edu/upeu/ui/presentation/components/LoadingCard.kt
@@ -0,0 +1,64 @@
+package pe.edu.upeu.ui.presentation.components
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+
+import androidx.compose.ui.unit.dp
+import com.valentinilk.shimmer.shimmer
+
+
+@Composable
+fun Spacer(size: Int = 8) = Spacer(modifier = Modifier.size(size.dp))
+@Composable
+fun ImageLoading() {
+    Box(modifier = Modifier.shimmer()) {
+        Box(
+            modifier = Modifier
+                .size(50.dp)
+                .background(Color.Gray)
+        )
+    }
+}
+@Composable
+fun LoadingCard() {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        elevation = 1.dp,
+        modifier = Modifier
+            .padding(horizontal = 8.dp, vertical = 4.dp)
+            .fillMaxWidth()
+            .testTag("loadingCard")
+    ) {
+        Row(modifier = Modifier.padding(8.dp)) {
+            ImageLoading()
+            Spacer()
+            Column {
+                Spacer()
+                Box(modifier = Modifier.shimmer()) {
+                    Column {
+                        Box(
+                            modifier = Modifier
+                                .height(15.dp)
+                                .fillMaxWidth()
+                                .background(Color.Gray)
+                        )
+                        Spacer()
+                        Box(
+                            modifier = Modifier
+                                .height(15.dp)
+                                .fillMaxWidth()
+                                .background(Color.Gray)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
