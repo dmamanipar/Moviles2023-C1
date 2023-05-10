8c600a8c392880746ec31f5655abd1df7608875b
Eddychp <74926092+Eddychp@users.noreply.github.com>
2023-05-10 14:48:31 -0500

    api

--- asistencia/app/Http/Controllers/FacultadController.php
+++ asistencia/app/Http/Controllers/FacultadController.php
@@ -12,94 +12,42 @@ class FacultaControllerd extends Controller
      * Display a listing of the resource.
      */
 
-    public function __construct() {
+     public function __construct() {
         $this->middleware('auth:api');
         }
 
-    public function index()
-    {
-        Log::channel('stderr')->info("Si llega aqui");
-
-        $facultades=Facultad::all();
-        $mappedcollection = $facultades->map(function($facultad, $key) {
-        return [
-        'id' => $facultad->id,
-        'nombrefac' => $facultad->nombrefac,
-        'estado'=>$facultad->estado,
-        'iniciales'=>$facultad->iniciales,
-
-        ];
-        });
-        return response()->json(['success' => true,
-        'data' => $mappedcollection,
-        //'data' => Persona::all(),
-        'message' => 'lista de facultades'], 200);
+    public function index(){
+        $facultads=Facultad::all();
+        //return $matriculas;
+        return response()->json($facultads);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+    public function show(matricula $facultad){
+        $matricula=matricula::find($matricula);
+        return response()->json($matricula);
+    }
+    public function store(MatriculaPostRequest $request){
+        $matricula = Matricula::create($request->all());
+
+        return response()->json([
+            'message' => "record saved successfully!",
+            'name' => $matricula
+        ], 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        $input = $request->all();
-        Log::channel('stderr')->info($request);
-        Facultad::create($input);
-        return response()->json(['success' => true,
-        'data' => Facultad::all(),
-        'message' => 'Lista de Facultad'], 200);
+    public function update(MatriculaPostRequest $request, matricula $matricula){
+        $matricula->update($request->all());
+
+        return response()->json([
+            'message' => "record updated successfully!",
+            'name' => $matricula
+        ], 200);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        Log::channel('stderr')->info($request);
-        $input = $request->all();
-        $facultad=Facultad::find($id);
-        $facultad->nombrefac = $input['nombrefac'];
-        $facultad->estado = $input['estado'];
-        $facultad->iniciales = $input['iniciales'];
-
-        $facultad->save();
-        return response()->json(['success' => true,
-        'data' => Facultad::all(),
-        'message' => 'Lista de facultades'], 200);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        Log::channel('stderr')->info($id);
-        Facultad::find($id)->delete();
-        return response()->json(['success' => true,
-        'data' => Facultad::all(),
-        'message' => 'eliminado correctamente'], 200);
+    public function destroy(matricula $matricula){
+        $matricula->delete();
+        return response()->json([
+            'message' => "record deleted successfully!",
+        ], 200);
     }
 }
